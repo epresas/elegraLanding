29f6d699ce45d32253b93631cfc419530fd3821e
BANKINTER\SE06596 <ejpresas_atsistemas@bankinter.com>
2019-02-19 15:15:27 +0100

    clients section

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -6,21 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  iconPath: '../../../assets/icons/';
   socialBookmarks = [
     {
       name: 'Twitter',
-      imgPath: '../../../assets/icons/twitter-letter-logo.svg',
+      imgPath: './assets/icons/twitter-letter-logo.svg',
       href: 'https://twitter.com/elegrasys'
     },
     {
       name: 'Facebook',
-      imgPath: '../../../assets/icons//facebook-letter-logo.svg',
+      imgPath: './assets/icons/facebook-letter-logo.svg',
       href: 'https://www.facebook.com/elegrasys'
     },
     {
       name: 'Linkedin',
-      imgPath: '../../../assets/icons//linkedin-logo.svg',
+      imgPath: './assets/icons/linkedin-logo.svg',
       href: 'https://es.linkedin.com/company/elegra-systems-s-l-'
     }
 ];
